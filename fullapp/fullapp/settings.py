a0b2621ee843bbcaf8973f0ccfd2f4946a40921f
bikash <bikashbhandari715@gmail.com>
2021-10-21 14:32:08 +0545

    edits

--- fullapp/fullapp/settings.py
+++ fullapp/fullapp/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-=4y4@%o42ywg0_)omuc$ski$#-_$qdsmjy41raq!ejnhe$khi2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
